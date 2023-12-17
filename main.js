238333f1ff6aa431ff986b63989449fd26476d61
spectralvlad <spectralvlad@gmail.com>
2023-12-17 05:39:17 +0200

    first commit

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-// Приклад 1
+Приклад 1
+
+function memoize(defaultValue) {
+  let memoizedData = defaultValue;
+
+  function getMemData() {
+    return memoizedData;
+  }
+
+  function setMemData(newData) {
+    memoizedData = newData;
+  }
+
+  return [getMemData, setMemData];
+}
+
+// Приклад використання:
 const memData = memoize();
 const getMemData = memData[0];
 const setMemData = memData[1];
@@ -6,15 +22,34 @@ const setMemData = memData[1];
 console.log(getMemData()); // поверне undefined
 setMemData(3);
 console.log(getMemData()); // 3
-setMemData('some string');
-setMemData({ a: 'b' });
+setMemData("some string");
+setMemData({ a: "b" });
 console.log(getMemData()); // поверне { a: 'b' }
 
-// Приклад 2
-const memData2 = memoize([1, 2, 3]);
-const getMemData2 = memData2[0];
-const setMemData2 = memData2[1];
 
-console.log(getMemData2()); // поверне [1, 2, 3]
-setMemData2('test');
-console.log(getMemData2()); // поверне 'test'
+
+
+Приклад 2
+
+function memoize(defaultValue) {
+  let memoizedData = Array.isArray(defaultValue) ? [...defaultValue] : defaultValue;
+
+  function getMemData() {
+    return memoizedData;
+  }
+
+  function setMemData(newData) {
+    memoizedData = newData;
+  }
+
+  return [getMemData, setMemData];
+}
+
+// Приклад використання:
+const memData = memoize([1, 2, 3]);
+const getMemData = memData[0];
+const setMemData = memData[1];
+
+console.log(getMemData()); // поверне [1, 2, 3]
+setMemData('test');
+console.log(getMemData()); // поверне 'test'
